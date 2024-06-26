da4e3b46584ed8bb24714fd96ae3be77f7d4c6ec
Seth Davison <sdavison@gravic.com>
2024-06-26 17:13:49 -0400

    Delete connection status component

--- signalr-client/src/client/App.tsx
+++ signalr-client/src/client/App.tsx
@@ -1,15 +1,13 @@
 import React from "react";
 import SignalRProvider from "./SignalRContext";
-import ConnectionStatus from "./ConnectionStatus";
 import TodoStore from "./TodoStore";
 import TodoList from "./TodoList";
-import '../../public/style.css'
+import "../../public/style.css";
 
 const App: React.FC = () => {
   return (
     <div className="APP justify-center">
       <SignalRProvider>
-        <ConnectionStatus />
         <TodoStore>{(todos) => <TodoList todos={todos} />}</TodoStore>
       </SignalRProvider>
     </div>

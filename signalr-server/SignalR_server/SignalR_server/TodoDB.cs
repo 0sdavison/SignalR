7622f9522340359e46c8338168f589151ebff79d
cfernandes <cfernandes@Gravic.onmicrosoft.com>
2024-06-26 14:00:09 -0400

    Added DB reference

--- signalr-server/SignalR_server/SignalR_server/TodoDB.cs
+++ signalr-server/SignalR_server/SignalR_server/TodoDB.cs
@@ -1,10 +1,17 @@
 ﻿//This is the database
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
 using Microsoft.EntityFrameworkCore;
 
 class TodoDb : DbContext
 {
-    public TodoDb(DbContextOptions<TodoDb> options)
-        : base(options) { }
+    string _connectionString = "Server=LAPTOP-BTOESM68;Database=gravhack;Username=postgres;Password=postgres;";
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(_connectionString);
+    }
 
-    public DbSet<Todo> Todos => Set<Todo>();
+    //public TodoDb(DbContextOptions<TodoDb> options)
+    //    : base(options) { }
+
+    //public DbSet<Todo> Todos => Set<Todo>();
 }

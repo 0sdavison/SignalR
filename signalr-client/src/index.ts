c7d502cc08823b5e6665fe94a56f1b83743352c7
Seth Davison <sdavison@gravic.com>
2024-06-26 10:56:51 -0400

    Install client dependencies

--- signalr-client/src/index.ts
+++ signalr-client/src/index.ts
@@ -1,34 +1,17 @@
-import * as signalR from "@microsoft/signalr";
-import "./css/main.css";
+import express, { Request, Response, NextFunction } from "express";
 
-const divMessages: HTMLDivElement = document.querySelector("#divMessages");
-const tbMessage: HTMLInputElement = document.querySelector("#tbMessage");
-const btnSend: HTMLButtonElement = document.querySelector("#btnSend");
-const username = new Date().getTime();
+const app = express();
 
-const connection = new signalR.HubConnectionBuilder().withUrl("/hub").build();
-
-connection.on("messageReceived", (username: string, message: string) => {
-  const m = document.createElement("div");
-
-  m.innerHTML = `<div class="message-author">${username}</div><div>${message}</div>`;
-
-  divMessages.appendChild(m);
-  divMessages.scrollTop = divMessages.scrollHeight;
-});
-
-connection.start().catch((err) => document.write(err));
-
-tbMessage.addEventListener("keyup", (e: KeyboardEvent) => {
-  if (e.key === "Enter") {
-    send();
+app.get("/", (req: Request, res: Response, next: NextFunction): void => {
+  try {
+    res.send("Hello World");
+  } catch (error) {
+    next(error);
   }
 });
 
-btnSend.addEventListener("click", send);
+const PORT = 3000;
 
-function send() {
-  connection
-    .send("newMessage", username, tbMessage.value)
-    .then(() => (tbMessage.value = ""));
-}
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});

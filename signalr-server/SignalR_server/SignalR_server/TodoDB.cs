64c011b0c0cf42fb0c78e9a6b297cdf9a0752df5
Bathos <gegoderod@yahoo.de>
2024-06-26 23:40:32 -0400

    now connects to database using nps

--- signalr-server/SignalR_server/SignalR_server/TodoDB.cs
+++ signalr-server/SignalR_server/SignalR_server/TodoDB.cs
@@ -1,17 +1,90 @@
 ﻿//This is the database
 using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
 using Microsoft.EntityFrameworkCore;
+using Npgsql;
+using Npgsql.EntityFrameworkCore.PostgreSQL;
+using System.Data.Common;
+using System.Reflection.Metadata;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
 
 class TodoDb : DbContext
 {
-    string _connectionString = "Server=LAPTOP-BTOESM68;Database=gravhack;Username=postgres;Password=postgres;";
+    string _connectionString = "Host=LAPTOP-BTOESM68;Port=5432;Database=gravhack;User id=postgres;Password=postgres;";
+    
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseSqlServer(_connectionString);
+        optionsBuilder.UseNpgsql(_connectionString);
+  //      DbDataSource dataSource;
+    //    dataSource.OpenConnection(_connectionString);
     }
-
+    public DbSet<Todo> ToDoTable { get; set; }
+ //   public virtual DbSet<Blog> Blogs { get; set; }
     //public TodoDb(DbContextOptions<TodoDb> options)
     //    : base(options) { }
 
     //public DbSet<Todo> Todos => Set<Todo>();
 }
+
+
+class DBInterface
+{
+
+    Todo GetTodo(int id)
+    {
+        TodoDb myDBcon = new TodoDb();
+        Todo SelectedTodo = new Todo();
+
+        //       myDBcon.ToDoTable.AsSingleQuery
+        var query = from b in myDBcon.ToDoTable
+                        orderby b.Name
+                        select b;
+        foreach (var item in query)
+        {
+            if (item.Id == id)
+            {
+                SelectedTodo.Id = item.Id;
+                SelectedTodo.Name = item.Name;
+                SelectedTodo.IsComplete = item.IsComplete;
+                Console.WriteLine(item.Name);
+            }
+        }
+
+
+        return SelectedTodo;
+    }
+}
+
+/*
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConsoleApp.PostgreSQL
+{
+    public class BloggingContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseNpgsql("Host=my_host;Database=my_db;Username=my_user;Password=my_pw");
+    }
+
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string Url { get; set; }
+
+        public List<Post> Posts { get; set; }
+    }
+
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public int BlogId { get; set; }
+        public Blog Blog { get; set; }
+    }
+}*/
